25d47ae00392fdb1178de77e821c57b5f58f5220
eliboa <romain@laumonier.org>
2016-02-02 21:11:02 +0100

    Release #1

--- src/loader.h
+++ src/loader.h
@@ -1,57 +1,11 @@
-#ifndef SNAKE_H
-#define SNAKE_H
+#ifndef LOADER_H
+#define LOADER_H
 #include "../../../../../../../devkitPro/libwiiu/src/coreinit.h"
 #include "../../../../../../../devkitPro/libwiiu/src/vpad.h"
 #include "../../../../../../../devkitPro/libwiiu/src/draw.h"
+#include "../../../../../../../devkitPro/libwiiu/src/string.h"
 
-typedef struct SnakeI SnakeI;
-struct SnakeI {
-	// Coordinates
-    int x;
-    int y;
-    int length;
-    char direction;
-    // Colors
-    int r;
-    int g;
-    int b;
+void printU(int line, char messageU[]);
+void _getIN();
 
-    int end;
-
-    SnakeI *next;
-};
-
-typedef struct Snake Snake;
-struct Snake {
-	int frame_x;
-	int frame_y;
-	int x;
-	int y;
-	int w;	
-	int length;
-	char direction;
-	int loose;
-    int pause;
-	int score;
-    int start;
-	int food_state;
-	int food_x;
-	int food_y;
-    int seed;
-	int debug;
-
-	SnakeI *first;
-};
-
-void initSnake(Snake* s);
-void displayStart(Snake* s);
-void displaySnake(Snake* s);
-void moveSnake(Snake* s, char direction);
-int isLooseMoove(Snake* s);
-void setNewFoodCoord(Snake* s);
-void triggerSnake(Snake* s, VPADData* vpad);
-void stretchSnake(Snake* s, int length);
-int randU(int range_max, Snake* s);
-
-
-#endif /* SNAKE_H */
+#endif /* LOADER_H */
