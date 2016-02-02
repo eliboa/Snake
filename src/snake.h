6c734cd4ee563ec76c158c4cb38dd4acb82439fa
eliboa <romain@laumonier.org>
2016-02-02 21:12:57 +0100

    Release #1

--- src/snake.h
+++ src/snake.h
@@ -33,39 +33,18 @@ struct Snake {
 	int loose;
     int pause;
 	int score;
-
+    int start;
 	int food_state;
 	int food_x;
 	int food_y;
-
     int seed;
-
 	int debug;
 
 	SnakeI *first;
 };
-typedef signed long long s64;
-typedef unsigned int     u32;
-typedef s64 OSTime;
-typedef u32 OSTick;
-typedef struct OSCalendarTime
-{
-    int sec;    // seconds after the minute [0, 61]
-    int min;    // minutes after the hour [0, 59]
-    int hour;   // hours since midnight [0, 23]
-    int mday;   // day of the month [1, 31]
-    int mon;    // month since January [0, 11]
-    int year;   // years since 0000
-    int wday;   // days since Sunday [0, 6]
-    int yday;   // days since January 1 [0, 365]
-
-    int msec;   // milliseconds after the second [0,999]
-    int usec;   // microseconds after the millisecond [0,999]
-} OSCalendarTime;
-
-
 
 void initSnake(Snake* s);
+void displayStart(Snake* s);
 void displaySnake(Snake* s);
 void moveSnake(Snake* s, char direction);
 int isLooseMoove(Snake* s);
@@ -73,7 +52,6 @@ void setNewFoodCoord(Snake* s);
 void triggerSnake(Snake* s, VPADData* vpad);
 void stretchSnake(Snake* s, int length);
 int randU(int range_max, Snake* s);
-OSCalendarTime* getDate();
 
 
 #endif /* SNAKE_H */
